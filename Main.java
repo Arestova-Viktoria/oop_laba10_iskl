c1266cf05fead979798155927f4c046aadff6c25
Viktoria <vikaarestova01@gmail.com>
2021-01-20 19:42:48 +0700

    classes java

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str_1 = "Slastena";
+        String str_2 = "Russia";
+        int m = 250;
+        int mil = 100;
+        int ex = 60;
+        int ch = 1;
+        int x = 12;
+        int y = 10;
+        int k = 2020;
+
+        Icecream ice1 = new Icecream();
+        ice1.Init_icecream_in();
+        ice1.Display();
+    }
+}
